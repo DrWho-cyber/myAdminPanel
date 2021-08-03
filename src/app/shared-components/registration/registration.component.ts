0f174c02dae78b1c55357b2889590ba7341168b6
tkemaladze98 <tkemaladze.1998@gmail.com>
2021-08-03 22:39:06 +0400

    implement firebase to register user

--- src/app/shared-components/registration/registration.component.ts
+++ src/app/shared-components/registration/registration.component.ts
@@ -1,25 +1,29 @@
-import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { User } from 'src/app/models/user.model';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {  User } from 'src/app/models/user.model';
+import { CrudServicesService } from 'src/app/service/crud-services.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
+  providers: [CrudServicesService]
 })
 export class RegistrationComponent implements OnInit {
-  userName!:string;
-  userSurName!:string;
-  userEmail!:string;
-  userPassword!:string;
-  userAge!:number;
-  userGender!:any;
-  
+  userName!: string;
+  userSurName!: string;
+  userEmail!: string;
+  userPassword!: string;
+  userAge!: number;
+  userGender!: any;
+
 
   @ViewChild('fileInput') fileInput!: ElementRef;
-  picturesArr:any[]  = [];
+  picturesArr: any[] = [];
+  user:User = new User ("","","","","",0,0,[])
 
   uploadFileEvt(imgFile: any) {
-    
+
     if (imgFile.target.files && imgFile.target.files[0]) {
       let self = this;
       for (let i = 0; i < imgFile.target.files.length; i++) {
@@ -28,30 +32,34 @@ export class RegistrationComponent implements OnInit {
         reader.onload = function () {
           self.picturesArr.push(reader.result);
         }
+      }
     }
-  }
-    
+
   }
 
-  
-  constructor() { }
+
+  constructor(private firebase: CrudServicesService) { }
 
   ngOnInit(): void {
   }
-  
-  onSubmit(){
-    let user:User = new User(
-      "", 
-      this.userName, 
-      this.userSurName, 
-      this.userEmail, 
+
+  onSubmit() {
+     var user:User = new User(
+      "",
+      this.userName,
+      this.userSurName,
+      this.userEmail,
       this.userPassword,
       this.userAge,
-      this.userGender, 
+      this.userGender,
       this.picturesArr
-      )
-      
-      
-      console.log(user)
+    )
+
+    console.log(user)
+    this.firebase.createUser(JSON.parse(JSON.stringify(user)))
+      .then((response: any) => {
+        
+      })
   }
+  
 }
