afd71d047daa4560bea7fd7070b84b3b25211244
tkemaladze98 <tkemaladze.1998@gmail.com>
2021-08-04 19:43:14 +0400

    add functions to get collections from firestore

--- src/app/views/side-nav-bar/users/users.component.ts
+++ src/app/views/side-nav-bar/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { CrudServicesService } from 'src/app/service/crud-services.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  allUsers:User[] =[];
+  constructor( private firebase:CrudServicesService) { }
 
   ngOnInit(): void {
+    this.firebase.readAllUsers().subscribe((response: any) => {
+      this.allUsers = [];
+      response.forEach((element: any) => {
+        var user = element.payload.doc.data();
+        user.key = element.payload.doc.id;
+        this.allUsers.push(user);
+      });
+    });
   }
+
 
 }
