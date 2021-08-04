b307782345a70c94c71218cf2062ef22bcf1b04e
DrWho-cyber <77809141+DrWho-cyber@users.noreply.github.com>
2021-08-04 13:48:46 +0400

    sign in field added

--- src/app/shared-components/autorisation/autorisation.component.ts
+++ src/app/shared-components/autorisation/autorisation.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autorisation.component.css']
 })
 export class AutorisationComponent implements OnInit {
-
+  userEmail!: string;
+  userPassword!: string;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onbtnClick(){
+    
   }
 
 }
