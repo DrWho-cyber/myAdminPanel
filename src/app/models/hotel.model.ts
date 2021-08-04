852a49da01fd3ac2205f23fe151f5b08a297f84a
DrWho-cyber <77809141+DrWho-cyber@users.noreply.github.com>
2021-08-04 15:30:18 +0400

    hotel model improved

--- src/app/models/hotel.model.ts
+++ src/app/models/hotel.model.ts
@@ -1,8 +1,36 @@
+export enum Status {
+    user = 0,
+    VIP = 1,
+}
+
+export enum Stars {
+    one = 0,
+    two = 1,
+    three = 3,
+    four = 4,
+    five = 5,
+}
+
 export class Hotel{
     public key!:string;
     public name!:string;
-    public adress!:string;
-    public city!:string;
-    public price!:string;
-    public imageUrl!:string;
+    public address!:string;
+    public phone!:number;
+    public email!:string;
+    public stars!:Stars;
+    public profilePicture!:string;
+    public status!:Status;
+
+    constructor(key:string, name:string, address:string,phone:number,email:string,stars:Stars,status:Status){
+     this.key = key;
+     this.name = name;
+     this.address = address;
+     this.phone = phone;
+     this.email = email;
+     this.stars = stars;
+     this.profilePicture = this.profilePicture;
+     this.status = status;
+    }
+
+
 }
