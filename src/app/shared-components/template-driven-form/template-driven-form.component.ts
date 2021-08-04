b48f080654361f0d4522436e0efbbecf5b39e243
tkemaladze98 <tkemaladze.1998@gmail.com>
2021-08-04 18:56:52 +0400

    Create Hotel and link it to firebase

--- src/app/shared-components/template-driven-form/template-driven-form.component.ts
+++ src/app/shared-components/template-driven-form/template-driven-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Hotel } from 'src/app/models/hotel.model';
+import { CrudServicesService } from 'src/app/service/crud-services.service';
 @Component({
   selector: 'app-template-driven-form',
   templateUrl: './template-driven-form.component.html',
@@ -8,13 +9,17 @@ import { Hotel } from 'src/app/models/hotel.model';
 })
 export class TemplateDrivenFormComponent implements OnInit {
 
-  constructor() { }
+  constructor( private firebase:CrudServicesService) { }
 
   ngOnInit(): void {
   }
 
   onFormSubmit(form:NgForm){
     var item = form.value as Hotel;
+    this.firebase.createHotel(JSON.parse(JSON.stringify(item)))
+      .then((response: any) => {
+        
+      })
     console.log(item);
   }
 }
