2ee992259b57d27d371011a3699938cfa74a22a9
DrWho-cyber <77809141+DrWho-cyber@users.noreply.github.com>
2021-08-04 16:50:09 +0400

    tamplateDriven form improved, hotelclassmodel improved

--- src/app/models/hotel.model.ts
+++ src/app/models/hotel.model.ts
@@ -4,8 +4,8 @@ export enum Status {
 }
 
 export enum Stars {
-    one = 0,
-    two = 1,
+    one = 1,
+    two = 2,
     three = 3,
     four = 4,
     five = 5,
@@ -14,22 +14,22 @@ export enum Stars {
 export class Hotel{
     public key!:string;
     public name!:string;
-    public address!:string;
+    public address!:{};
     public phone!:number;
     public email!:string;
     public stars!:Stars;
-    public profilePicture!:string;
     public status!:Status;
+    public profilePicture!:string;
 
-    constructor(key:string, name:string, address:string,phone:number,email:string,stars:Stars,status:Status){
+    constructor(key:string, name:string, address:{},phone:number,email:string,stars:Stars,status:Status){
      this.key = key;
      this.name = name;
      this.address = address;
      this.phone = phone;
      this.email = email;
      this.stars = stars;
-     this.profilePicture = this.profilePicture;
      this.status = status;
+     this.profilePicture = this.profilePicture;
     }
 
 
